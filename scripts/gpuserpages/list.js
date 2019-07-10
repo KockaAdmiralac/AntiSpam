5c2c2964465f3c5cb314c2348ddd053c13f207ad
unknown <njojbihvise@ilovemail.be>
2019-07-10 20:02:07 +0200

    user page links → contribution links

--- scripts/gpuserpages/list.js
+++ scripts/gpuserpages/list.js
@@ -3,7 +3,7 @@ module.exports = function(wiki) {
     require('fs').writeFileSync(
         'links.txt',
         require('./links-final.json')
-            .map(l => `https://${wiki}/User:${l}`)
+            .map(l => `https://${wiki}/Special:Contributions/${l}`)
             .join('\n')
     );
 };
--- scripts/userpages/list.js
+++ scripts/userpages/list.js
@@ -3,7 +3,7 @@ module.exports = function(wiki) {
     require('fs').writeFileSync(
         'links.txt',
         require('./links-final.json')
-            .map(l => `* [[w:c:${wiki}:User:${l}|${l}]] ([[w:c:${wiki}:Special:Contribs/${l}|c]])`)
+            .map(l => `https://${wiki}/Special:Contributions/${l}`)
             .join('\n')
     );
 };
